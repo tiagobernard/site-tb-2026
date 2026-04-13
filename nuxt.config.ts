15e205d470bca7f92bf2545e346db22994d034a9
Tiago Bernardes <tiagobernard@iMac-de-Tiago.local>
2026-04-13 20:25:52 -0300

    fix: Corrigido bug de carregamento de fonts no CSS Main.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,18 @@ export default defineNuxtConfig({
   css: ['~/assets/css/main.css'],
 
   modules: ['@nuxt/ui', '@pinia/nuxt'],
+
+  app: {
+    baseURL: '/',
+    head: {
+      link: [
+        { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
+        { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap' }
+      ]
+    }
+  },
+
   future: {
     compatibilityVersion: 4
   }
