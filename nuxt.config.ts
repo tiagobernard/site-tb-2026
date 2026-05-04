31bd5107813202ae968b9a0d73abb3f74a4dd54f
Tiago Bernardes <tiago@tiagobernardes.com.br>
2026-05-04 12:46:37 -0300

    feat: Modernização do favicon para PNG.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,7 +29,8 @@ export default defineNuxtConfig({
       link: [
         { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
         { rel: 'preconnect', href: 'https://fonts.gstatic.com', crossorigin: '' },
-        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600;700;800&display=swap' }
+        { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&family=JetBrains+Mono:wght@400;500;600;700;800&display=swap' },
+        { rel: 'icon', type: 'image/png', href: '/favicon.png' }
       ]
     }
   },
